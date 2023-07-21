54d957bdbded48132f114a7769e48f82558b58a0
[Niranjan Malla] <[mallaniranjan08@gmail.com]>
2023-07-21 18:58:23 +0100

    installation of livewire and npm is done

---
+++ app/Http/Livewire/WebFrontend/HomeComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\WebFrontend;
+
+use Livewire\Component;
+
+class HomeComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.web-frontend.home-component');
+    }
+}
